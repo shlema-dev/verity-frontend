8a1e73e565200a279e509a6430a6fd3eef6fccac
shlema-dev <shlema.dev@gmail.com>
2024-06-06 15:31:30 -0400

    Start contact form

--- app/contact/page.tsx
+++ app/contact/page.tsx
@@ -1,3 +1,14 @@
+import ContactForm from "@/components/contact/ContactForm";
+
 export default function ContactPage() {
-  return <section></section>;
+  return (
+    <div className="w-full h-full max-w-7xl mb-12 flex flex-col">
+      <section className="flex flex-col gap-6 lg:gap-12 justify-center lg:items-center">
+        <h1 className="text-center mt-8 text-5xl text-gray-12 font-semibold">
+          Get in <span className="text-primary-9">Contact</span>
+        </h1>
+      </section>
+      <ContactForm />
+    </div>
+  );
 }
---
+++ components/contact/ContactForm.tsx
@@ -0,0 +1,57 @@
+"use client";
+
+import { Field, Input, Label, Textarea } from "@headlessui/react";
+
+const ContactForm: React.FC = () => {
+  return (
+    <form className="w-full max-w-2xl mt-12 p-4 flex flex-col gap-4 border border-primary-8 rounded-xl self-center">
+      <Field>
+        <Label className="text-sm/6 font-medium text-primary-12">
+          First Name
+        </Label>
+        <Input
+          type="text"
+          id="firstname"
+          name="firstname"
+          className={`mt-3 block w-full rounded-lg border-none bg-primary-3 outline outline-2 -outline-offset-2 outline-primary-6 py-1.5 px-3 text-sm/6 text-primary-12
+            focus:outline-none data-[focus]:outline-2 data-[focus]:-outline-offset-2 data-[focus]:outline-primary-8`}
+        />
+      </Field>
+      <Field>
+        <Label className="text-sm/6 font-medium text-primary-12">
+          Last Name
+        </Label>
+        <Input
+          type="text"
+          id="lastname"
+          name="lastname"
+          className={`mt-3 block w-full rounded-lg border-none bg-primary-3 outline outline-2 -outline-offset-2 outline-primary-6 py-1.5 px-3 text-sm/6 text-primary-12
+            focus:outline-none data-[focus]:outline-2 data-[focus]:-outline-offset-2 data-[focus]:outline-primary-8`}
+        />
+      </Field>
+      <Field>
+        <Label className="text-sm/6 font-medium text-primary-12">
+          Email Address
+        </Label>
+        <Input
+          type="text"
+          id="email"
+          name="email"
+          className={`mt-3 block w-full rounded-lg border-none bg-primary-3 outline outline-2 -outline-offset-2 outline-primary-6 py-1.5 px-3 text-sm/6 text-primary-12
+            focus:outline-none data-[focus]:outline-2 data-[focus]:-outline-offset-2 data-[focus]:outline-primary-8`}
+        />
+      </Field>
+      <Field>
+        <Label className="text-sm/6 font-medium text-primary-12">Message</Label>
+        <Textarea
+          name="message"
+          rows={5}
+          className={`mt-3 block w-full rounded-lg border-none bg-primary-3 outline outline-2 -outline-offset-2 outline-primary-6 py-1.5 px-3 text-sm/6 text-primary-12
+            focus:outline-none data-[focus]:outline-2 data-[focus]:-outline-offset-2 data-[focus]:outline-primary-8`}
+        />
+      </Field>
+    </form>
+  );
+};
+
+export default ContactForm;
