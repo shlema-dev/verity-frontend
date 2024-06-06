693be86510ec07c40720e96eccf3f7ac5c2663d1
shlema-dev <shlema.dev@gmail.com>
2024-06-06 15:53:30 -0400

    Add form button

--- components/contact/ContactForm.tsx
+++ components/contact/ContactForm.tsx
@@ -1,10 +1,10 @@
 "use client";
 
-import { Field, Input, Label, Textarea } from "@headlessui/react";
+import { Button, Field, Input, Label, Textarea } from "@headlessui/react";
 
 const ContactForm: React.FC = () => {
   return (
-    <form className="w-full max-w-2xl mt-12 p-4 flex flex-col gap-4 border border-primary-8 rounded-xl self-center">
+    <form className="w-full max-w-2xl mt-12 p-4 lg:p-8 flex flex-col gap-4 border border-primary-8 rounded-xl self-center">
       <Field>
         <Label className="text-sm/6 font-medium text-primary-12">
           First Name
@@ -50,6 +50,11 @@ const ContactForm: React.FC = () => {
             focus:outline-none data-[focus]:outline-2 data-[focus]:-outline-offset-2 data-[focus]:outline-primary-8`}
         />
       </Field>
+      <Button
+        className={`rounded-lg bg-primary-10 mt-4 py-2 px-4 text-primary-1 dark:text-primary-12 data-[hover]:bg-primary-9 `}
+      >
+        Submit
+      </Button>
     </form>
   );
 };
